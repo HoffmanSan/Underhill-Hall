b1edb8b49600a763f83b180b2fa4f9efebcb47af
HoffmanSan <133865170+HoffmanSan@users.noreply.github.com>
2023-09-27 00:38:39 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader("Access-Control-Allow-Headers", "*");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
   next();
 });
 
